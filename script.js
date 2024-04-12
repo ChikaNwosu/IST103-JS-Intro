777ff3204612e0517523ed99da6090a204c5c89f
ChikaNwosu <165509397+ChikaNwosu@users.noreply.github.com>
2024-04-11 18:47:13 -0700

    Updates new changes

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-//alert(
-    "Hello World";
+//Activity 1
+//alert("Hello World");
+
+
+//Activity 2
 let personName = "Jimsy";
 let personAge = 30;
 const eyecolour = "brown";
-let employed = true;
+let isMarried = false;
+let isEmployed = true;
+
+console.log(personName + " has a " + eyecolour + " eyes.");
+
+if(!isMarried){
+    console.log("Get marry with me!")
+} else {
+    console.log("I love you!")
+}
+
+//Activity 3
+let todolist = ["move out", "find a house", "surf the internet for vacancies", "get pillows", "get cleaning supplies"];
+
+let myFragrance = {
+    brand: "Japanese Cherry Blossom",
+    Type: "Bath and Body Works",
+    Kind: "Body Mist",
+    colour: "Cherry Red",
+    isUsed: "true",
+    numberUsed: 20
+};
+
+console.log("my first to do is: " + todolist[0]);
+console.log("my fragrance brand is: " + myFragrance.brand);
